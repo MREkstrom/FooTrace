2d66a3a2b0500225d420ed9e3632603e3a4d548c
rstudio <rstudio@example.com>
2016-11-27 12:08:45 -0500

    Dan Cuthbert
    Color Picker added through Alert Dialog
    Clearing Path added through Alert Dialog
    Saving Added through Alert Dialog

--- app/src/main/java/com/example/michael/footrace/DesignView.java
+++ app/src/main/java/com/example/michael/footrace/DesignView.java
@@ -1,30 +1,77 @@
-package com.example.michael.footrace;
+package com.example.michael.footrace;;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
-
-/**
- * Created by JonSeibert on 11/23/16.
- */
 
 public class DesignView extends View {
 
+    private Path _path = new Path();
+    private Paint _paint = new Paint();
+    private float _prevPoint = -1;
+
     public DesignView(Context context) {
         super(context);
-//        init(null, 0);
+        init(null, 0);
     }
 
     public DesignView(Context context, AttributeSet attrs) {
         super(context, attrs);
-//        init(attrs, 0);
+        init(attrs, 0);
     }
 
     public DesignView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-//        init(attrs, defStyle);
+        init(attrs, defStyle);
     }
 
-    // TODO - implement DesignView content to allow a user to draw a path to trace
+    private void init(AttributeSet attrs, int defStyleAttr){
+        _paint.setColor(Color.BLACK);
+        _paint.setStyle(Paint.Style.STROKE);
+        _paint.setStrokeWidth(12);
+    }
+
+    public void setBrushColor(int red, int green, int blue){
+        _paint.setColor(Color.rgb(red,green,blue));
+        invalidate();
+    }
+
+    public void clear() {
+        _path.reset();
+        _prevPoint = -1;
+        invalidate();
+    }
+
+    public void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+        canvas.drawPath(_path,_paint);
+    }
+
+    public boolean onTouchEvent(MotionEvent motionEvent){
+        float touchX = motionEvent.getX();
+        float touchY = motionEvent.getY();
+
+        switch(motionEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                if(_prevPoint != -1){
+                    _path.lineTo(touchX,touchY);
+                }
+                _path.moveTo(touchX,touchY);
+            case MotionEvent.ACTION_MOVE:
+                _path.lineTo(touchX,touchY);
+            case MotionEvent.ACTION_UP:
+                _path.moveTo(touchX,touchY);
+                _prevPoint = touchX;
+        }
+
+        invalidate();
+        return true;
+    }
 
 }
--- app/src/main/java/com/example/michael/footrace/NewDesign.java
+++ app/src/main/java/com/example/michael/footrace/NewDesign.java
@@ -1,13 +1,28 @@
 package com.example.michael.footrace;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewDesign extends AppCompatActivity {
 
     DesignView mDesignView;
+    private int _red = 0;
+    private int _green = 0;
+    private int _blue = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,25 +34,186 @@ public class NewDesign extends AppCompatActivity {
 
     public void onDesignButtonClick(View v){
 
+        AlertDialog.Builder builder;
+        LayoutInflater inflater;
+        AlertDialog alertDialog;
+
         switch(v.getId()){
             case R.id.colorButton:
+                final int temp_red = _red;
+                final int temp_green = _green;
+                final int temp_blue = _blue;
 
-                // TODO - open color popup, change color
+                //builds alert dialog for clearing
+                builder = new AlertDialog.Builder(this);
 
-                break;
+                // Get the layout inflater. LayoutInflaters take a layout XML file and create its
+                // corresponding View objects. Never create LayoutInflaters directly. Always use the
+                // factory method getLayoutInflater. See https://developer.android.com/reference/android/view/LayoutInflater.html
+                inflater = this.getLayoutInflater();
+
+                // Inflate the dialog_color.xml layout and create the View
+                final View colorDialogView = inflater.inflate(R.layout.dialog_color, null);
+
+                //Sets color swatch to selected path color
+                TextView swatch = (TextView)colorDialogView.findViewById(R.id.colorSwatch);
+                swatch.setBackgroundColor(Color.rgb(_red,_green,_blue));
+
+                // Get access to the seekbar on this dialog.
+                SeekBar seekBar = (SeekBar)colorDialogView.findViewById(R.id.seekBarRed);
+
+                // Set progress bar to current color value
+                seekBar.setProgress(_red);
+
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    //Updates red to value shown on seekbar
+                    //Updates swatch in real time
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        _red = progress;
+                        TextView swatch = (TextView)colorDialogView.findViewById(R.id.colorSwatch);
+                        swatch.setBackgroundColor(Color.rgb(_red,_green,_blue));
+                    }
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) { }
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {  }
+                });
+
+                // Get access to the seekbar on this dialog.
+                seekBar = (SeekBar)colorDialogView.findViewById(R.id.seekBarGreen);
+
+                // Set progress bar to current color value
+                seekBar.setProgress(_green);
+
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        //Updates green to value shown on seekbar
+                        //Updates swatch in real time
+                        _green = progress;
+                        TextView swatch = (TextView)colorDialogView.findViewById(R.id.colorSwatch);
+                        swatch.setBackgroundColor(Color.rgb(_red,_green,_blue));
+                    }
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) { }
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {  }
+                });
+
+                // Get access to the seekbar on this dialog.
+                seekBar = (SeekBar)colorDialogView.findViewById(R.id.seekBarBlue);
+
+                // Set progress bar to current color value
+                seekBar.setProgress(_blue);
+
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        //Updates blue to value shown on seekbar
+                        //Updates swatch in real time
+                        _blue = progress;
+                        TextView swatch = (TextView)colorDialogView.findViewById(R.id.colorSwatch);
+                        swatch.setBackgroundColor(Color.rgb(_red,_green,_blue));
+                    }
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) { }
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {  }
+                });
+
+                // This is the method that allows us to use our own custom view. We set the AlertDialog builder
+                // to the view we created with the inflater above.
+                builder.setView(colorDialogView)
+                        // Add action buttons
+                        .setTitle("Color Picker")
+                        .setPositiveButton("OK!", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                //Updates brush to correct color
+                                mDesignView.setBrushColor(_red,_green,_blue);
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //resets colors to former values
+                                _red = temp_red;
+                                _green = temp_green;
+                                _blue = temp_blue;
+                            }
+                        });
+
+                alertDialog = builder.create();
+                alertDialog.show();
+
             case R.id.clearButton:
+                //builds alert dialog for clearing
+                builder = new AlertDialog.Builder(this);
 
-                // TODO - clear the design via dialog popup
+                // Get the layout inflater. LayoutInflaters take a layout XML file and create its
+                // corresponding View objects. Never create LayoutInflaters directly. Always use the
+                // factory method getLayoutInflater. See https://developer.android.com/reference/android/view/LayoutInflater.html
+                inflater = this.getLayoutInflater();
 
-                break;
+                // Inflate the dialog_color.xml layout and create the View
+                final View clearDialogView = inflater.inflate(R.layout.dialog_clear, null);
+
+                builder.setView(clearDialogView)
+
+                        .setTitle("Clear Path")
+                        .setMessage("Do you want to clear your path?")
+                        .setPositiveButton("OK!", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id){
+                                mDesignView.clear();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                alertDialog = builder.create();
+                alertDialog.show();
+
             case R.id.saveButton:
-                Intent resultIntent = new Intent();
+                //builds alert dialog for saving
+                builder = new AlertDialog.Builder(this);
+
+                // Get the layout inflater. LayoutInflaters take a layout XML file and create its
+                // corresponding View objects. Never create LayoutInflaters directly. Always use the
+                // factory method getLayoutInflater. See https://developer.android.com/reference/android/view/LayoutInflater.html
+                inflater = this.getLayoutInflater();
+
+                // Inflate the dialog_save.xml layout and create the View
+                final View saveDialogView = inflater.inflate(R.layout.dialog_save, null);
+
+                builder.setView(saveDialogView)
+                        // Add action buttons
+                        .setTitle("Save Your Design")
+                        .setPositiveButton("OK!", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                //save path and name
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                alertDialog = builder.create();
+                alertDialog.show();
 
                 // TODO - save the design into resultIntent via dialog popup
+                Intent resultIntent = new Intent();
 
                 setResult(RESULT_OK, resultIntent);
                 finish();
-                break;
+
             case R.id.mapToggleButton:
 
                 // TODO - add/remove map to DesignView background
