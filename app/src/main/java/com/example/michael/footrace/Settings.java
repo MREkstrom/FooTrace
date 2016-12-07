c96665a7a7ef1e477e1fb571a11e684188ebb7d6
Dan-Cuthbert <dcuthber@terpmail.umd.edu>
2016-12-06 21:19:50 -0500

    Button style corrected

--- app/src/main/java/com/example/michael/footrace/Settings.java
+++ app/src/main/java/com/example/michael/footrace/Settings.java
@@ -4,11 +4,13 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -46,6 +48,13 @@ public class Settings extends AppCompatActivity {
 
         TextView emailView = (TextView)findViewById(R.id.emailAddress);
         emailView.setText(_email);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "mvboli.ttf");
+        Button[] buttons = {(Button) findViewById(R.id.editButton),
+                (Button) findViewById(R.id.backButton)};
+        for (Button b : buttons) {
+            b.setTypeface(tf);
+        }
 
         final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
